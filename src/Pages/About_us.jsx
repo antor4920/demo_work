e2091644a2fe7e6b063ed362f0ca4e16e99c774a
antor4920 <antor4920@gmail.com>
2025-02-02 13:11:03 +0600

    L

--- src/Pages/About_us.jsx
+++ src/Pages/About_us.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { useFormik } from "formik";
+
+const Input_Text = () => {
+  const formik = useFormik({
+    initialValues: {
+      companyname: "",
+      position: "",
+      details: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.onSubmit}>
+        <div>
+          <label>Company Name:</label>
+          <input
+            type="text"
+            id="companyname"
+            name="companyname"
+            value={formik.values.companyname}
+            onChange={formik.handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Position:</label>
+          <input
+            type="text"
+            id="position"
+            name="position"
+            value={formik.values.position}
+            onChange={formik.handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Details:</label>
+          <input
+            type="text"
+            id="details"
+            name="details"
+            value={formik.values.details}
+            onChange={formik.handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 const About_us = () => {
-  return <div>About_us</div>;
+  return (
+    <div>
+      <Input_Text />
+    </div>
+  );
 };
 
 export default About_us;
